0e96843c44cd0551ca52d55a1f5442707f629617
Daniela Strauss <dani_strau@yahoo.de>
2020-01-15 14:15:20 +0100

    commit

--- resources/views/überuns.blade.php
+++ resources/views/überuns.blade.php
@@ -7,24 +7,22 @@
                 <div class="aboutme ">
                     <img src="{{ asset('/assets/dani.jpg') }}" />
                     <p class="main-p">
-                        Daniela Strauss studiert an der SAE Wien Web Design &
-                        Development. Neben dem Studium ist sie als Student
-                        Support Kraft in der Raiffeisen
-                        Kapitalanlage-Gesellschaft m.b.H. tätig. Dort
-                        unterstützt sie bei der Bearbeitung und Betreuung der
-                        rcm.at Webseite. Bevor sie diesen Lebensabschnitt
-                        begonnen hat, hat sie circa vier Jahre als
-                        Kindergruppenbetreuerin gearbeitet. Zu ihren Tätigkeiten
+                        Ich, Daniela Strauss, studiere an der SAE Wien Web Design &
+                        Development. Neben dem Studium bin ich als Student
+                        Support Kraft in der Raiffeisen Kapitalanlage-Gesellschaft tätig. Dort
+                        unterstützte ich bei der Bearbeitung und Betreuung der
+                        rcm.at Webseite. Bevor ich diesen Lebensabschnitt
+                        begonnen habe, habe ich circa vier Jahre als
+                        Kindergruppenbetreuerin gearbeitet. Zu meinen Tätigkeiten
                         gehörten, neben der Betreuung der Kinder, lernen,
                         singen, spielen, basteln und malen mit den Kleinen.
-                        Zusätzlich hat sie ehrenamtlich, für eine Organisation,
+                        Zusätzlich habe ich ehrenamtlich, für eine Organisation,
                         mit Word Press gearbeitet und auch verschiedenen Social
                         Media Kanäle wie Facebook, Twitter und Instagram
-                        betreut. Durch diese Erfahrungen hat sie das Interesse
-                        für die Branche entwickelt und somit entschieden ihre
+                        betreut. Durch diese Erfahrungen habe ich das Interesse
+                        für die Branche entwickelt und somit entschieden meine
                         berufliche Laufbahn zu ändern und an der SAE Wien
-                        begonnen zu studieren. Daniela ist 29 Jahre jung,
-                        verlobt und wohnhaft in Wien.
+                        begonnen zu studieren. Ich bin 29 Jahre jung und wohnhaft in Wien.
                     </p>
                 </div>
             </div>
