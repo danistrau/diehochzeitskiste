6f9e7f3c335d74fd581217b6b77a8123e0979c9d
Daniela Strauss <dani_strau@yahoo.de>
2020-01-27 14:20:57 +0100

    greensock

--- public/js/main.js
+++ public/js/main.js
@@ -63,7 +63,10 @@ Sticky NAV
 
     var tl = new TimelineLite();
 
-    //tl.from(".main-content", 2, { x: -1500 });
-    //tl.from(".main-text", 2, { x: -1500 });
+    tl.staggerFrom("section", 2, { x: -2000, opacity:1,}, 0.25);
+
+    tl.staggerFrom(".main-budgetrechner .budgetrechner li", 3, { x: -1500, opacity:1,}, 0.5);
+    tl.staggerFrom(".checklist_item", 3, { x: -1500, opacity:1,}, 0.25);
+    
     
 });
