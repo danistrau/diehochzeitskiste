427dc5443a281e5cae1d778ce0f19dba6c5bba4f
Daniela Strauss <dani_strau@yahoo.de>
2020-02-03 13:44:33 +0100

    footer fixed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,53 +5,6 @@
         </div>
         <div class="footer-content">
             <div>
-                <a href="{{ url('/') }}">
-                    <svg
-                        class="footerlogo"
-                        version="1.1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        x="0"
-                        y="0"
-                        viewBox="0 0 160.8 41.5"
-                        xml:space="preserve"
-                    >
-                        <style>
-                            .st0 {
-                                fill: none;
-                                stroke: #e4dadb;
-                                stroke-miterlimit: 10;
-                            }
-                        </style>
-                        <g>
-                            <path
-                                class="st0"
-                                d="M11.6 12.2h30.8v26H11.6v-26z"
-                            />
-                            <path
-                                d="M30.1 37.9l-6.2.1V12.6l6.1-.1v25.4h.1zM25.2 1.8L1.6 20.9l3.8 4.3L28.9 6.1l-3.7-4.3z"
-                                fill="#e4dadb"
-                                stroke="#e4dadb"
-                                stroke-miterlimit="10"
-                            />
-                            <path
-                                class="st0"
-                                d="M13.4 11.4S9.4 6 5.1 9.9s2.5 7.4 8.3 1.5z"
-                            />
-                            <path
-                                class="st0"
-                                d="M13.4 11.4S8 7.1 13 3.3s6.9 4.3.4 8.1z"
-                            />
-                            <text
-                                transform="matrix(.88 0 0 1 44.174 38.179)"
-                                font-size="20"
-                                font-family="HelveticaNeue"
-                                fill="#e4dadb"
-                            >
-                                Hochzeitskiste
-                            </text>
-                        </g>
-                    </svg></a
-                ><br />
                 <small>&copy; Design & Development by Daniela Strauss</small>
             </div>
             <div class="footer-text">
